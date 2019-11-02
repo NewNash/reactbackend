7b91ff7b270b4a1346c4e407694fbb81f396ca3a
nash <545100402@qq.com>
2019-11-02 15:02:02 +0800

    完善搜索条件查询

--- src/contentlist.js
+++ src/contentlist.js
@@ -1,84 +1,84 @@
 import React from "react";
-import {Table, Divider, Tag, Icon, Popconfirm, message,Switch} from "antd";
+import {Table, Divider, Tag, Icon, Popconfirm, message, Switch} from "antd";
 import {connect} from 'react-redux'
 import moment from "moment";
 import {Link} from "react-router-dom";
 import axios from 'axios'
 import SearchBox from "./searchbox";
+
 class ContentList extends React.Component {
     componentDidMount() {
-        // this.props.dispatch({type: 'contentlist'})
+        this.props.dispatch({type: 'contentlist'})
     }
-
     render() {
         const columns = [
             {
                 title: '发布时间',
-                align:'center',
+                align: 'center',
                 dataIndex: 'date',
                 key: 'publishDate',
                 render: a => moment(a.$date).format('YYYY-MM-DD HH:mm:ss')
             },
             {
                 title: '修改时间',
-                align:'center',
+                align: 'center',
                 dataIndex: 'changeDate',
                 key: 'modifyDate',
                 render: a => moment(a.$date).format('YYYY-MM-DD HH:mm:ss')
             },
             {
                 title: '标题',
-                align:'center',
+                align: 'center',
                 dataIndex: 'title',
                 key: 'title',
                 render: a => a
             },
             {
                 title: '目录',
-                align:'center',
+                align: 'center',
                 dataIndex: 'category',
                 key: 'category',
                 render: a => a
             },
             {
                 title: '二级目录',
-                align:'center',
+                align: 'center',
                 dataIndex: 'subCategory',
                 key: 'subCategory',
                 render: a => a
             },
             {
                 title: 'tag',
-                align:'center',
+                align: 'center',
                 dataIndex: 'tag',
                 key: 'tag',
                 render: tags => (
                     <span>
-                        {tags.map(tag => (<Tag key={tag} color='#2db7f5' style={{fontSize:'14px'}}>{tag}</Tag>))}
+                        {tags.map(tag => (<Tag key={tag} color='#2db7f5' style={{fontSize: '14px'}}>{tag}</Tag>))}
                     </span>
                 )
             },
             {
                 title: '操作',
-                align:'center',
+                align: 'center',
                 // dataIndex:'operate',
                 key: 'operate',
                 render: (record, b) => {
                     const delconfirm = () => {
-                        axios.post('https://stayalone.cn/delarticle',{_id:record._id.$oid}).then((res)=>{
-                            if(res.data==='ok'){
+                        axios.post('https://stayalone.cn/delarticle', {_id: record._id.$oid}).then((res) => {
+                            if (res.data === 'ok') {
                                 message.success('文章删除成功')
                                 window.location.reload()
                             }
                         })
                     }
-                    const switchChange=(ifshow)=>{
+                    const switchChange = (ifshow) => {
                         // console.log(record)
-                        axios.post('https://stayalone.cn/showarticle',{
-                            _id:record._id.$oid,
-                            show:ifshow,
-                        }).then((res)=>{
-                             if(res.data==='ok'){
+                        axios.post('https://stayalone.cn/showarticle', {
+                            _id: record._id.$oid,
+                            show: ifshow,
+                        }).then((res) => {
+                            if (res.data === 'ok') {
                                 message.success('修改成功')
                                 window.location.reload()
                             }
@@ -86,7 +86,8 @@ class ContentList extends React.Component {
                     }
                     return (
                         <span>
-                            <Switch checkedChildren="显示" unCheckedChildren="隐藏" size='small' defaultChecked={record.show} onChange={switchChange}/>
+                            <Switch checkedChildren="显示" unCheckedChildren="隐藏" size='small'
+                                    defaultChecked={record.show} onChange={switchChange}/>
                             <Divider type='vertical'/>
                             <Link to={{pathname: '/admin/modify-article', state: {text: record}}}>
                                 <Icon type="edit" title='编辑文章'/>
@@ -96,7 +97,7 @@ class ContentList extends React.Component {
                                         onConfirm={delconfirm}
                                         okText="Yes"
                                         cancelText="No">
-                                <Icon type="delete" title='删除文章' style={{color: 'red'}} />
+                                <Icon type="delete" title='删除文章' style={{color: 'red'}}/>
                             </Popconfirm>
                         </span>
                     )
@@ -104,7 +105,13 @@ class ContentList extends React.Component {
             },
         ]
         const pagination = {
-            total:this.props.contentlist.count
+            total: this.props.contentlist.count,
+            onChange: (page, pagesize) => {
+                // this.props.dispatch({type:'pagechaneg',text:{key1:'pageNo',key1Value:page,key2:'pageSize',key2Value:pagesize,}})
+                this.props.dispatch({type: 'searchOptionChange', text: {key: 'pageNo', value: page}})
+                this.props.dispatch({type: 'searchOptionChange', text: {key: 'pageSize', value: pagesize}})
+                this.props.dispatch({type: 'contentlist'})
+            }
         }
         return (
             <div style={{width: '80%'}}>
@@ -121,6 +128,7 @@ class ContentList extends React.Component {
 
 const mapStateToProps = (state) => ({
     contentlist: state.contentlist,
+    contentSearchOption: state.contentSearchOption
 })
 const mapDispatchToProps = (dispatch) => ({
     dispatch
